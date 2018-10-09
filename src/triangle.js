b69514e867a8aa5945d368efc9382a4294920ff2
Mark Mangahas and Skye Nguyen <skye@>
2018-10-09 09:38:36 -0700

    add logic

--- src/triangle.js
+++ src/triangle.js
@@ -1,15 +1,22 @@
-export function pingPong(goal) {
-  var output = [];
-  for (var i = 1; i <= goal; i++) {
-    if (i % 15 === 0) {
-      output.push("ping-pong");
-    } else if (i % 3 === 0) {
-      output.push("ping");
-    } else if (i % 5 === 0) {
-      output.push("pong");
-    } else {
-      output.push(i);
-    }
-  }
-  return output;
+export function Triangle(side1, side2, side3) {
+  this.side1 = side1;
+  this.side2 = side2;
+  this.side3 = side3;
 }
+
+Triangle.prototype.checkType = function() {
+  if ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) {
+    return "not a triangle";
+
+  }  else if ((this.side1 === this.side2) && (this.side2 === this.side3)) {
+    return "an equilateral triangle";
+
+  } else if ((this.side1 === this.side2) || ((this.side1 === this.side3)) || ((this.side2 === this.side3))) {
+    return "an isosceles triangle";
+
+  } else if ((this.side1 !== this.side2) && ((this.side1 !== this.side3)) && ((this.side2 !== this.side3))) {
+    return "a scalene triangle";
+  } else {
+    return "unable to compute";
+  }
+};
