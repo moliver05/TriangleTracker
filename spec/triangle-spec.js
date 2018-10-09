25bf4811b9c6a4f5e0f1ad48a35ae4934125ceda
Mark Mangahas and Skye Nguyen <skye@>
2018-10-09 09:38:25 -0700

    add tests

---
+++ spec/triangle-spec.js
@@ -0,0 +1,42 @@
+import { Triangle } from './../src/triangle.js';
+
+describe('Triangle', function()
+{
+    var reusableTriangle;
+    beforeEach(function(){
+        reusableTriangle = new Triangle(5,5,5);
+    });
+   
+    it('should correctly determine if a triangle is equilateral', function() {
+        expect(reusableTriangle.checkType()).toEqual('an equilateral triangle');
+    });
+
+    it('should test whether a Triangle has three sides', function()
+    {
+        var triangle = new Triangle(3,4,5);
+        expect(triangle.side1).toEqual(3);
+        expect(triangle.side2).toEqual(4);
+        expect(triangle.side3).not.toEqual(6);
+
+    });
+    
+    it('should correctly determine whether three lengths can be made into a triangle', function() {
+        var notTriangle = new Triangle(3,9,22);
+        expect(notTriangle.checkType()).toEqual("not a triangle");
+    
+    });
+
+    it('should correctly determine whether three lengths can be made into a triangle', function () {
+        var isocTriangle = new Triangle (5,5,7);
+        expect(isocTriangle.checkType()).toEqual("an isosceles triangle");
+    });
+
+    it('should correctly determine whethere three lengths can be made into triangle', function () {
+        var scalTriangle = new Triangle(4,5,7);
+        expect(scalTriangle.checkType()).toEqual("a scalene triangle");
+    });
+
+
+
+});
+
